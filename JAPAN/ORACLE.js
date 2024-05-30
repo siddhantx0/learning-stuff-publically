d919cc4e3d94bd86138c8c1aada7417410581580
Siddhant Singh <singhsiddhantakshat@gmail.com>
2024-05-30 03:23:51 -0500

    Update ORACLE.js

--- JAPAN/ORACLE.js
+++ JAPAN/ORACLE.js
@@ -1,4 +1,9 @@
 {
 	("Protype.apply");
+	("Array.prototype.includes");
 	("https://ecma-international.org/wp-content/uploads/ECMA-262_13th_edition_june_2022.pdfs");
+	Object.values;
+	Object.entries;
+	Object.getOwnPropertyDescriptor;
+	("dotall/spread operators/unicode property escapes/look behind assertions");
 }
