2407d5d587a193c373baf8bd02ea98d452ae9330
Siddhant Singh <singhsiddhantakshat@gmail.com>
2024-05-30 03:57:03 -0500

    Update ORACLE.js

--- JAPAN/ORACLE.js
+++ JAPAN/ORACLE.js
@@ -1,10 +1,28 @@
+console.table(
+	"Protype.apply",
+	"Array.prototype.includes",
+	"https://ecma-international.org/wp-content/uploads/ECMA-262_13th_edition_june_2022.pdfs",
+	Object.values,
+	Object.entries,
+	Object.getOwnPropertyDescriptor,
+	"dotall/capture properties/spread operators/unicode property escapes/look behind assertions",
+	2019,
+	flat,
+	flatMap,
+	Array.prototype("for flattening arrays"),
+	Object.fromEntries,
+	Object.entries,
+	String.prototype.trimStart,
+	String.prototype.trimEnd,
+	Array.prototype.sort,
+	JSON.stringify
+);
+
+2019;
 {
-	("Protype.apply");
-	("Array.prototype.includes");
-	("https://ecma-international.org/wp-content/uploads/ECMA-262_13th_edition_june_2022.pdfs");
-	Object.values;
-	Object.entries;
-	Object.getOwnPropertyDescriptor;
-	("dotall/spread operators/unicode property escapes/look behind assertions");
-	("#buy beo vcoin#");
+	Function.prototype.toString();
+}
+
+2020;
+{
 }
